ce186b102e4af74f29a9b836c7872dcdad0d9d6c
shinyveemo <84742754+shinyveemo@users.noreply.github.com>
2021-09-22 14:15:11 -0400

    Create Garbage.java

---
+++ Garbage.java
@@ -0,0 +1,11 @@
+/* File:            garbage.java
+ * Description:     Prints out that I can upload to the repository.
+ * Author:          Samantha Maddox
+ */
+
+public class Garbage() {
+    public static void main(String[] args) {
+        System.out.println("I can upload to the repository");
+        System.exit(1);
+    }
+}
