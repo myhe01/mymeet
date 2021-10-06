f9a27326e60db01a290b35a7c4c2ab4667eda65a
Brendan College <cpbro123@gmail.com>
2021-10-06 13:13:46 -0400

    Py email

---
+++ Examples/BrendanHelloWorld.java
@@ -0,0 +1,14 @@
+/*  
+-   File name:      BrendanHelloWorld.java
+-
+-   Purpose:        Prints "Hello World!" to stdout, then exits.
+-
+-   Methods:        None
+*/
+
+public class BrendanHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.exit(1);
+    }
+}
