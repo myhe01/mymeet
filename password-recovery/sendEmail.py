b9a85b69dbd0bff8c71f47e5ad099da60b68f633
Brendan College <cpbro123@gmail.com>
2021-10-08 12:08:15 -0400

    Added minor exception handling

--- password-recovery/sendEmail.py
+++ password-recovery/sendEmail.py
@@ -27,8 +27,11 @@ msg.attach(MIMEText(body.read(), 'html'))
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login("dev.mymeet@gmail.com", password)
-    server.sendmail("dev.mymeet@gmail.com", to, msg.as_string())
-    server.quit()
+    try:
+        server.login("dev.mymeet@gmail.com", password)
+        server.sendmail("dev.mymeet@gmail.com", to, msg.as_string())
+        server.quit()
+    except Exception as e:
+        print(e)
 
 exit()
