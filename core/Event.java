e0531229b73db6ead475123201058649d7456a60
Alejandro Fernandez <alejandro.fernandez@knights.ucf.edu>
2021-11-19 20:14:54 -0500

    added event duration

--- core/Event.java
+++ core/Event.java
@@ -7,16 +7,19 @@ public class Event {
     private String eventLocation;
     private String eventDate;
     private String eventTime;
+    private Integer eventDuration;
     
     public Event() {}
 
-    public Event(String eventID, String groupID, String eventLocation, String eventDate, String eventTime)
+    public Event(String eventID, String groupID, String eventLocation,
+                 String eventDate, String eventTime, Integer eventDuration)
     {
         this.setEventID(eventID);
         this.setGroupID(groupID);
         this.setEventLocation(eventLocation);
         this.setEventDate(eventDate);
         this.setEventTime(eventTime);
+        this.setEventDuration(eventDuration);
     }
 
     // Getters&&Setters
@@ -25,9 +28,11 @@ public class Event {
     public String getGroupID() { return groupID; }
     public void setGroupID(String groupID) { this.groupID = groupID; }
     public String getEventLocation() { return eventLocation; }
-    public void setEventLocation(String eventLocation) {this.eventLocation = eventLocation; }
+    public void setEventLocation(String eventLocation) { this.eventLocation = eventLocation; }
     public String getEventDate() { return eventDate; }
     public void setEventDate(String eventDate) { this.eventDate = eventDate; }
-    public String getEventTime() {return eventTime; }
-    public void setEventTime(String eventTime) {this.eventTime = eventTime; }
+    public String getEventTime() { return eventTime; }
+    public void setEventTime(String eventTime) { this.eventTime = eventTime; }
+    public Integer getEventDuration() { return eventDuration; }
+    public void setEventDuration(Integer eventDuration) { this.eventDuration = eventDuration; }
 }
