93367d17fe1ec3a3257e740d7f6a3dd676d9c96e
Alejandro Fernandez <alejandro.fernandez@knights.ucf.edu>
2021-11-29 08:21:09 -0500

    Updated databaseConnection

--- query/databaseConnect.java
+++ query/databaseConnect.java
@@ -5,5 +5,28 @@
 import java.sql.*;
 
 public class databaseConnect {
-	//TODO fill in
+	public String dbUrl = ""; // database URL
+    public String dbName = "root"; // name of database
+    public String dbPassword = ""; // password to database (often left either blank or root)
+    public String dbDriver = ""; // LDBC "com.mysql.jdbc.Driver
+
+    public void loadDriver(String dbDriver)
+    {
+        try {
+            Class.forName(dbDriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection()
+    {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbName, dbPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
