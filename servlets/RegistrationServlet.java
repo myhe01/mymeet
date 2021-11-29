b8ff7288d9ae17f8dee9ddc00ac7d1940e9eed7e
Alejandro Fernandez <alejandro.fernandez@knights.ucf.edu>
2021-11-29 08:22:30 -0500

    Updated Servlets

--- servlets/RegistrationServlet.java
+++ servlets/RegistrationServlet.java
@@ -7,21 +7,45 @@ import javax.servlet.http.*;
 
 @WebServlet(
         name = "registration",
-        urlPatterns = "/registration/"
+        urlPatterns = "/registration"
 )
-public class RegistrationServlet extends HttpServlet {
+public class RegistrationServlet extends HttpServlet, Query {
 
     private String message;
     private static final long serialVersionUID = 1L;
 
     // Required method to initialize Servlet
     public void init() throws ServletException{
-        message = "Welcome to MyMeet";
+
     }
 
     // Required method to accept the GET request
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+
+//        response.setContentType("text/html");
+//        PrintWriter printWriter = response.getWriter();
+//        printWriter.print("<html>");
+//        printWriter.print("<body>");
+//        printWriter.print("<h1>Student Registration Form Data</h1>");
+//        printWriter.print("<p> firstName :: " + firstName + "</p>");
+//        printWriter.print("<p> lastName :: " + lastName + "</p>");
+//        printWriter.print("<p> userName :: " + userName + "</p>");
+//        printWriter.print("<p> userEmail :: " + userEmail + "</p>");
+//        printWriter.print("<p> password :: " + password + "</p>");
+//        printWriter.print("<body>");
+//        printWriter.print("<html>");
+//        printWriter.close();
+//
+//        System.out.println("firstname :: " + firstName);
+//        System.out.println("email :: " + userEmail);
+
+    }
+
+    // Required method to accept a POST request
+    //
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
@@ -29,28 +53,14 @@ public class RegistrationServlet extends HttpServlet {
         String userEmail = request.getParameter("userEmail");
         String password = request.getParameter("password");
 
-        response.setContentType("text/html");
-        PrintWriter printWriter = response.getWriter();
-        printWriter.print("<html>");
-        printWriter.print("<body>");
-        printWriter.print("<h1>Student Registration Form Data</h1>");
-        printWriter.print("<p> firstName :: " + firstName + "</p>");
-        printWriter.print("<p> lastName :: " + lastName + "</p>");
-        printWriter.print("<p> userName :: " + userName + "</p>");
-        printWriter.print("<p> userEmail :: " + userEmail + "</p>");
-        printWriter.print("<p> password :: " + password + "</p>");
-        printWriter.print("<body>");
-        printWriter.print("<html>");
-        printWriter.close();
+        User newUser = new User(firstName, lastName, userName, userEmail, password);
+        databaseConnect dbQuery = new databaseConnect();
 
-        System.out.println("firstname :: " + firstName);
-        System.out.println("email :: " + userEmail);
-    }
-
-    // Required method to accept a POST request
-    //
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+        try {
+            Query.addUser(newUser);
+        } catch (UserNotFound e){
+            e.printStackTrace;
+        }
 
     }
     public void destroy() {
